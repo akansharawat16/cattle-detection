938bdd4ceb18869cb8ba7c29a6abb7a63fd4c282
Akansha Rawat <104184424+akansharawat16@users.noreply.github.com>
2024-08-16 12:52:12 +0530

    Create ty1.py

---
+++ ty1.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
+# Use the model
+results = model.train(data="D:\Major project files\cattle-detection-1\data.yaml", epochs=10)  # train the model 
